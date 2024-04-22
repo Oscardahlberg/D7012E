fe17ef9b08db3feece86b274d679eaef072313a2
Oscardahlberg <31594957+Oscardahlberg@users.noreply.github.com>
2024-04-22 13:34:53 +0200

    Added comments to prolog lab 2

--- Prolog/lab2.pl
+++ Prolog/lab2.pl
@@ -22,6 +22,8 @@ kSmallestSubArrays(Arr, K, KSmallestSorted) :-
     length(SubLists, Size),
     smallestSubset(SubLists, Size, K, KSmallestSorted).
 
+% Different predicates for the size of the sublist
+% After if the list is larger than 1 it is sorted
 smallestSubset(SubLists, Size, _, Sorted) :-
     Size =:= 1,
     Sorted = SubLists.
@@ -30,16 +32,21 @@ smallestSubset(SubLists, Size, K, Sorted) :-
     Size =:= K,
     insertionSort(SubLists, Sorted).
 
+% Sorts the list and then only returns the k smallest
 smallestSubset(SubLists, Size, K, KSmallest) :-
     Size > K,
     insertionSort(SubLists, Sorted),
     kSmallest(Sorted, K, KSmallest).
 
+% Adds the k first elements in the list to an new list
+% through recursion, then returns it.
 kSmallest([First|_], 1, [First]).
 kSmallest([First|RestArr], K, [First|Larger]) :-
     Rest is K - 1,
     kSmallest(RestArr, Rest, Larger).
 
+% Standard insertion sort
+% Sorts based on the size of Sum
 insertionSort([], []).
 insertionSort([X|Rest], Sorted) :-
     insertionSort(Rest, SortedRest),
@@ -52,6 +59,12 @@ insert((Sum, Arr, Idx), [(SumY, ArrY, IdxY)|Rest], [(SumY, ArrY, IdxY)|Sorted]) 
     Sum > SumY,
     insert((Sum, Arr, Idx), Rest, Sorted).
 
+% The elements in the array looks like this: (Sum, List, (First index, Last index))
+% The objective of this function is to get all sub lists from the list
+% It does this by calling PrefixIndexSubLists with -1 to the LastIndex
+% so it removes the last element.
+% It then recursively applies -1 to it until it is of size 1 and returns all
+% Then fullSubLists calls itself with +1 to FirstIndex and then prefixIndexSublists again
 fullSubLists([LastItem], FIdx, LIdx, [(LastItem, [LastItem], (FIdx, LIdx))]).
 fullSubLists(Arr, FirstIndex, LastIndex, [(Sum, Arr, (FirstIndex, LastIndex))| NewState]) :-
     sumList(Arr, Sum),
@@ -70,12 +83,15 @@ prefixIndexSubLists(Arr, FirstIndex, LastIndex, [(Sum, Arr, (FirstIndex, LastInd
     NewLast is LastIndex - 1,
     prefixIndexSubLists(Head, FirstIndex, NewLast, PrefixSubLists).
 
+% Removes the last element (index List size element)
 removeLast([_], []).
 removeLast([X|Xs], [X|Rest]) :-
     removeLast(Xs, Rest).
 
+% Removes the First element, (index 0 element)
 removeFirst([_|Xs], Xs).
 
+% Finds the sum of a list
 sumList([], 0).
 sumList([Head|Tail], Sum) :-
     sumList(Tail, TailSum),
